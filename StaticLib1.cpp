7771428c5140ec5deaec2789651363e26d986eda
mturshatova <mturshatova@ya.ru>
2024-02-18 18:56:46 +0300

    2

---
+++ StaticLib1.cpp
@@ -0,0 +1,42 @@
+﻿// StaticLib1.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Это пример библиотечной функции.
+void fnStaticLib1()
+{
+}
+
+bool checkPrimeNumber(int n) {
+
+	bool isPrimeNumber = true;
+
+	// 0 и 1 не являются простыми числами 
+
+	if (n == 0 || n == 1) {
+
+		isPrimeNumber = false;
+
+	}
+
+	else {
+
+		for (int i = 2; i <= n / 2; ++i) {
+
+			if (n % i == 0) {
+
+				isPrimeNumber = false;
+
+				break;
+
+			}
+
+		}
+
+	}
+
+	return isPrimeNumber;
+
+}
---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "../StaticLib1/framework.h";
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(checkPrimeNumber(3), true);
+  EXPECT_TRUE(true);
+}
